8f4c0e793e911ba8c70bf9bf44bdc0a88650949b
J Arthur Johnston <joe.a.johnston@gmail.com>
2015-08-17 16:10:02 -0400

    Added some example javascript code for accessing the api, and an integration test

---
+++ test/integration/url_responses_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class UrlResponsesTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user1 = User.create(name: 'John Doe')
+    @user2 = User.create(name: 'Jane Doe')
+
+    @timesheet1 = Timesheet.create(start_date: DateTime.new_starting(2015, 1,1), user_id: @user1.id)
+    @timesheet2 = Timesheet.create(start_date: DateTime.new_starting(2015, 1,1), user_id: @user1.id)
+    @timesheet3 = Timesheet.create(start_date: DateTime.new_starting(2015, 1,1), user_id: @user2.id)
+  end
+
+  test 'user index' do
+    get '/users'
+
+    assert_response :success
+    assert_equal [@user1, @user2].to_json, @response.body
+  end
+
+  test 'timesheet index' do
+    get '/usert/' + @user1.id.to_s + '/timesheets'
+
+    assert_response :success
+    assert_equal [@timesheet1, @timesheet2].to_json, @response.body
+  end
+
+
+end
